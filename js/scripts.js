d27ecbf7beacab6aa12622319a24ea639475532e
Dan Danilyuk and David Hale <dan@>
2017-08-16 09:43:29 -0700

    create initial files and function to identify vowel at beginning of string

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#pigForm").submit(function(event){
+    event.preventDefault();
+    var latin = $("input#pigLatin").val();
+    // var result = ;
+    // alert();
+    // $('#result').text(result);
+    pigLatin(latin);
+  });
+});
+
+var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u", "Y", "y"];
+var consonants = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'V', 'v', 'W', 'w', 'X', 'x', 'Z', 'z'];
+function pigLatin(str) {
+  for (var j = 0; j < vowels.length; j++) {
+    if (str.charAt(0) === vowels[j]) {
+      str = str.concat('way');
+      alert(str);
+      return;
+    }
+  }
+}
