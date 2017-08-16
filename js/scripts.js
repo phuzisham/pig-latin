40444b2ccd8bbb9cdbb5b47ba41a4626d8e29d94
Dan Danilyuk and David Hale <dan+david@>
2017-08-16 14:18:47 -0700

    add else if for y as consonant

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,36 @@ $(document).ready(function(){
   });
 });
 
-var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
-var consonants = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'];
+var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u", "Y", "y"];
+var consonants = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'V', 'v', 'W', 'w', 'X', 'x', 'Z', 'z'];
+
+function stringArray(str) {
+  str = str.split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = pigLatin(str[i]);
+  }
+  alert(str.join(' '));
+}
 
 function pigLatin(str) {
   for (var j = 0; j < consonants.length; j++) {
       if (str.slice(0, 2) == 'qu' || str.slice(0, 2) == 'Qu') {
         var x = str.slice(0, 2);
         str = str.slice(2).concat(x);
-        //alert(str);
+        return str;
+      } else if (str.charAt(0) === 'y' || str.charAt(0) === 'Y') {
+        var x = str.slice(0, 1);
+        str = str.slice(1).concat(x + 'ay');
         return str;
       } else if (str.charAt(0) === vowels[j]) {
       str = str.concat('way');
-      //alert(str);
-      return str;
+        return str;
       } else if (str.charAt(0) === consonants[j] && str.charAt(1) == 'q' && str.charAt(2) == 'u') {
         var x = str.slice(0, 3);
         str = str.slice(3).concat(x);
         return str;
       } else if (str.charAt(0) === consonants[j]) {
-          //alert(cons(str));
-          return cons(str);
+        return cons(str);
     }
   }
 }
@@ -42,12 +51,4 @@ function cons(str) {
       }
     }
   }
-}
-
-function stringArray(str) {
-  str = str.split(' ');
-  for (var i = 0; i < str.length; i++) {
-    str[i] = pigLatin(str[i]);
-  }
-  alert(str.join(' '));
 }
