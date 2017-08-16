60ac95f04dff9be6bc1fc75115a1da78b2530c33
Dan Danilyuk and David Hale <david@>
2017-08-16 10:41:15 -0700

    add function to concat consecutive consanants on end of string

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,32 @@ $(document).ready(function(){
   });
 });
 
-var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u", "Y", "y"];
-var consonants = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'V', 'v', 'W', 'w', 'X', 'x', 'Z', 'z'];
+var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+var consonants = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'];
+
+
 function pigLatin(str) {
-  for (var j = 0; j < vowels.length; j++) {
+  for (var j = 0; j < consonants.length; j++) {
     if (str.charAt(0) === vowels[j]) {
       str = str.concat('way');
       alert(str);
       return;
+    } else if (str.charAt(0) === consonants[j]) {
+      alert(cons(str));
+      return;
+    }
+  }
+}
+
+function cons(str) {
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < consonants.length; j++) {
+      if (str.charAt(0) === vowels[j]) {
+        return str.concat('ay');
+      } else if (str.charAt(0) == consonants[j]) {
+        var x = str.slice(0, 1);
+        str = str.slice(1).concat(x);
+      }
     }
   }
 }
